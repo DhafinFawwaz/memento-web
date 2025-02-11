d6079898b987ebfa2c3fe7ccfe9b6fcf69a156da
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2025-02-11 17:01:05 +0700

    fix: middleware cors

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
  
 export function middleware(request: NextRequest) {
@@ -8,6 +8,16 @@ export function middleware(request: NextRequest) {
             status: 401,
         });
     }
+
+    const res = NextResponse.next()
+    res.headers.append('Access-Control-Allow-Credentials', "true")
+    res.headers.append('Access-Control-Allow-Origin', '*')
+    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT,OPTIONS')
+    res.headers.append(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Authorisation, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    return res
 }
  
 export const config = {
