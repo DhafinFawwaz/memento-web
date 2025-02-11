c67bcd61d9dd8bf3bf751b0450165deb6f12df37
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2025-02-11 17:11:41 +0700

    fix: middleware response with ok

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 
  
 export function middleware(request: NextRequest) {
+    if (request.method === 'OPTIONS') {
+        return new Response("ok", {status: 200})
+    }
+
     const authHeader = request.headers.get('authorization');
     if (authHeader !== `Basic ${process.env.BASIC_AUTH_SECRET}`) {
         return new Response('Unauthorized', {
